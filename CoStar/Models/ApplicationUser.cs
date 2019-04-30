47fb1382f4acdcb27af51d5c6716655734974719
Zac Crawford <zwcrawford@gmail.com>
2019-04-30 12:26:15 -0500

    added nav and footer info, added all classes

---
+++ CoStar/Models/ApplicationUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CoStar.Models
+{
+	// Add profile data for application users by adding properties to the ApplicationUser class
+	public class ApplicationUser : IdentityUser
+	{
+		public ApplicationUser()
+		{
+
+		}
+		public string ApplicationUserImage { get; set; }
+
+		[Required]
+		[Display(Name = "First Name")]
+		public string FirstName { get; set; }
+
+		[Required]
+		[Display(Name = "Last Name")]
+		public string LastName { get; set; }
+
+		[Required]
+		override public string Email { get; set; }
+
+		public  DateTime EnrollDate { get; set; }
+	}
+}
+
---
+++ CoStar/Models/Whiteboard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoStar.Models
+{
+	public class Whiteboard
+	{
+		public string WhiteboardImage { get; set; }
+
+		[Required]
+		[Display(Name = "Whiteboard Name")]
+		public string WhiteboardName { get; set; }
+
+		[Required]
+		[Display(Name = "Whiteboard Description")]
+		public string WhiteboardDescription { get; set; }
+
+		public ApplicationUser User { get; set; }
+	}
+}
